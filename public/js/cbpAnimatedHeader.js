7f1b522c5a4b2b731363ed0227fc23ca4e85bc8f
nathaliesicard <nathalie.sicard@gmail.com>
2015-09-03 15:09:47 -0500

    Updated pictures.

--- public/js/cbpAnimatedHeader.js
+++ public/js/cbpAnimatedHeader.js
@@ -10,6 +10,8 @@
  */
 var cbpAnimatedHeader = (function() {
 
+	throw new Error('lol');
+
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
@@ -27,10 +29,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+		//	classie.add( header, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+		//	classie.remove( header, 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
